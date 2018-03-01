f63f616fac76617d65d7aa9507dd2d7668604452
suupp <36947379+suupp@users.noreply.github.com>
2018-03-01 11:37:05 +0400

    Add files via upload

---
+++ grid.js
@@ -0,0 +1,12 @@
+var drawGridRects = function(num_rectangles_wide, num_rectangles_tall, boundingRect) {
+    var width_per_rectangle = boundingRect.width / num_rectangles_wide;
+    var height_per_rectangle = boundingRect.height / num_rectangles_tall;
+    for (var i = 0; i < num_rectangles_wide; i++) {
+        for (var j = 0; j < num_rectangles_tall; j++) {
+            var aRect = new paper.Path.Rectangle(boundingRect.left + i * width_per_rectangle, boundingRect.top + j * height_per_rectangle, width_per_rectangle, height_per_rectangle);
+            aRect.strokeColor = 'black';
+            aRect.fillColor = 'white';
+        }
+    }
+
+}
