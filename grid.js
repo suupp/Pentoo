56c031c0e32a1bbcb165c198058b644fa7ccc996
suupp <36947379+suupp@users.noreply.github.com>
2018-03-01 16:42:39 +0400

    Merge branch 'master' of https://github.com/suupp/Pentoo

--- grid.js
+++ grid.js
@@ -1,6 +1,8 @@
+var width_per_rectangle;
+var height_per_rectangle;
 var drawGridRects = function(num_rectangles_wide, num_rectangles_tall, boundingRect) {
-    var width_per_rectangle = boundingRect.width / num_rectangles_wide;
-    var height_per_rectangle = boundingRect.height / num_rectangles_tall;
+     width_per_rectangle = boundingRect.width / num_rectangles_wide;
+     height_per_rectangle = boundingRect.height / num_rectangles_tall;
     for (var i = 0; i < num_rectangles_wide; i++) {
         for (var j = 0; j < num_rectangles_tall; j++) {
             var aRect = new paper.Path.Rectangle(boundingRect.left + i * width_per_rectangle, boundingRect.top + j * height_per_rectangle, width_per_rectangle, height_per_rectangle);
@@ -11,5 +13,6 @@ var drawGridRects = function(num_rectangles_wide, num_rectangles_tall, boundingR
 }
 
 var fillRect = function() {
-
+var eRect = new paper.Path.Rectangle(0,0,width_per_rectangle-1,height_per_rectangle-1);
+eRect.fillColor = 'red';
 }
