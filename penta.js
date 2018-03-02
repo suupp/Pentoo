1e50729b945f6c616eee1be4b9fc74dc8395728f
Yury Z <yury.zaytsev@gmail.com>
2018-03-02 12:44:52 +0400

    added pentas file, reset button and improved board appearance

---
+++ penta.js
@@ -0,0 +1,37 @@
+// declare array where we store penta figures
+var pentas = [];
+
+// fill in the array with pentomino figures per https://en.wikipedia.org/wiki/Pentomino#Symmetry
+
+// 4 vaiations of L figure 
+pentas.push([[0,0], [1,0], [2,0], [3,0], [3,1]]);
+pentas.push([[0,0], [0,1], [0,2], [0,3], [-1,3]]);
+pentas.push([[0,0], [0,1], [1,1], [2,1], [3,1]]);
+pentas.push([[0,0], [1,0], [0,1], [0,2], [0,3]]);
+
+// 4 vaiations of mirrored L figure 
+
+// 8 vaiations of F figure 
+
+// 8 vaiations of N figure 
+
+// 8 vaiations of P figure 
+
+// 8 vaiations of Y figure 
+
+// 4 vaiations of T figure 
+
+// 4 vaiations of U figure 
+
+// 4 vaiations of V figure 
+
+// 4 vaiations of W figure
+
+// 4 vaiations of Z figure
+
+// 2 vaiations of I figure
+
+// 1 vaiation of X figure
+
+//console.log("debug");
+//console.log(pentas);
